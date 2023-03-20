3a537ce92551e71e9860b589e60d59812778b746
gururajhavanur <115388867+gururajhavanur@users.noreply.github.com>
2023-03-21 01:32:17 +0530

    Create variables.tf

---
+++ create_ec2/variables.tf
@@ -0,0 +1,18 @@
+variable "ec2-type" {
+    type = string
+}
+
+variable "ami-id" {
+    type = string
+       
+}
+
+variable "ec2-pem" {
+    type = string
+        
+}
+
+variable "ec2_sg_id" {
+    type = string
+        
+}
