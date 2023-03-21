20fd9006d0b0e39d57dcd7687074ef97baa4c573
gururajhavanur <115388867+gururajhavanur@users.noreply.github.com>
2023-03-21 15:30:59 +0530

    Update main.tf

--- create_ec2/main.tf
+++ create_ec2/main.tf
@@ -1,10 +1,25 @@
-resource "aws_instance" "test1" {
-    ami= var.ami-id
-    instance_type = var.ec2-type
-    key_name = var.ec2-pem
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "my_ec2_instance" {
+    ami = data.aws_ami.ubuntu.id
+    instance_type = var.ec2_type
+    key_name = var.ec2_pem
     vpc_security_group_ids = [var.ec2_sg_id]
     tags = {
         Name = "Terraform"
     }
 }
- 
