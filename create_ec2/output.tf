295e91a63340e576471a86979fed7c8c53780048
gururajhavanur <115388867+gururajhavanur@users.noreply.github.com>
2023-03-21 15:31:30 +0530

    Update output.tf

--- create_ec2/output.tf
+++ create_ec2/output.tf
@@ -1,9 +1,3 @@
 output "ec2_public_ip_address" {
-    value = aws_instance.test1.public_ip
-
+    value = aws_instance.my_ec2_instance.public_ip
 }
-
-output "ec2_private_ip_address" {
-    value = aws_instance.test1.private_ip
-
-} 
